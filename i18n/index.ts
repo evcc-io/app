f8bc82562afd68ac2f2225ddb30657435c1f95bb
Maschga <delphi@freenet.de>
2024-12-24 09:58:20 +0100

    Update index.ts

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,15 +1,23 @@
 import { Resource } from "i18next";
 
-const files = require.context(
-    '.', // current directory
-    false, // search in subdirectories
-    /.+\.json$/ // only .json files
-);
+import de from "./de.json";
+import en from "./en.json";
+import fi from "./fi.json";
+import fr from "./fr.json";
+import lb from "./lb.json";
+import lt from "./lt.json";
+import pl from "./pl.json";
+import pt from "./pt.json";
 
-const translations: Resource = {};
-
-files.keys().forEach((fileName) => {
-    translations[fileName.replace('.json', '').replace('./', '')] = {translation: files(fileName)};
-});
+const translations: Resource = {
+  de: { translation: de },
+  en: { translation: en },
+  fi: { translation: fi },
+  fr: { translation: fr },
+  lb: { translation: lb },
+  lt: { translation: lt },
+  pl: { translation: pl },
+  pt: { translation: pt }
+};
 
 export default translations;
