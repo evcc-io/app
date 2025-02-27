8e41716a036b4796494d363f45d8600969349334
Michael Geers <michael@geers.tv>
2025-02-27 17:40:40 +0100

    add translations

--- i18n/index.ts
+++ i18n/index.ts
@@ -3,10 +3,13 @@ import { Resource } from "i18next";
 import ar from "./ar.json";
 import da from "./da.json";
 import de from "./de.json";
+import el from "./el.json";
 import en from "./en.json";
+import et from "./et.json";
 import fi from "./fi.json";
 import fr from "./fr.json";
 import hr from "./hr.json";
+import hu from "./hu.json";
 import lb from "./lb.json";
 import lt from "./lt.json";
 import nl from "./nl.json";
@@ -20,10 +23,13 @@ const translations: Resource = {
   ar: { translation: ar },
   da: { translation: da },
   de: { translation: de },
+  el: { translation: el },
   en: { translation: en },
+  et: { translation: et },
   fi: { translation: fi },
   fr: { translation: fr },
   hr: { translation: hr },
+  hu: { translation: hu },
   lb: { translation: lb },
   lt: { translation: lt },
   nl: { translation: nl },
