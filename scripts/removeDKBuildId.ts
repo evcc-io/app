ead8640ea141963a15205c4e151220b1fa698acd
Maschga <delphi@freenet.de>
2025-04-09 12:26:31 +0200

    wip

---
+++ scripts/removeDKBuildId.ts
@@ -0,0 +1,23 @@
+import { type ConfigPlugin } from "@expo/config-plugins";
+import { withAppBuildGradle } from "@expo/config-plugins/build/plugins/android-plugins.js";
+import type { ExpoConfig } from "@expo/config-types";
+
+const withNDKBuildId: ConfigPlugin = (config: ExpoConfig) => {
+  return withAppBuildGradle(config, (gradleConfig) => {
+    if (typeof gradleConfig.modResults.contents === "string") {
+      gradleConfig.modResults.contents =
+        gradleConfig.modResults.contents.replace(
+          "defaultConfig {",
+          `defaultConfig {
+            externalNativeBuild {
+                ndkBuild {
+                    arguments "APP_LDFLAGS=--build-id=none"
+                }
+            }`,
+        );
+    }
+    return gradleConfig;
+  });
+};
+
+export default withNDKBuildId;
