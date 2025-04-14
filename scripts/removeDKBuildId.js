e5cf28832e8183a0fa28f0a210ff8b81dc5e8f00
Maschga <88616799+Maschga@users.noreply.github.com>
2025-04-14 06:37:34 +0200

    Fix: Remove NDK Build ID (#63)

--- scripts/removeDKBuildId.js
+++ scripts/removeDKBuildId.js
@@ -1,24 +1,35 @@
-const {
-  withAppBuildGradle,
-} = require("@expo/config-plugins/build/plugins/android-plugins.js");
+// withAddLinkOption.js
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
-const withNDKBuildId = (config) => {
-  return withAppBuildGradle(config, (gradleConfig) => {
-    if (typeof gradleConfig.modResults.contents === "string") {
-      gradleConfig.modResults.contents =
-        gradleConfig.modResults.contents.replace(
-          "defaultConfig {",
-          `defaultConfig {
-            externalNativeBuild {
-                cmake {
-                    path "CMakeLists.txt"
-                }
-            }
-            `,
+const withAddLinkOption = (config) => {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const packages = ['react-native-screens', 'expo-modules-core'];
+      for (const pkg of packages) {
+        const cmakeFilePath = path.join(
+          config.modRequest.projectRoot,
+          'node_modules',
+          pkg,
+          'android',
+          'CMakeLists.txt'
         );
-    }
-    return gradleConfig;
-  });
+        if (fs.existsSync(cmakeFilePath)) {
+          let contents = fs.readFileSync(cmakeFilePath, 'utf8');
+          // Insert the linker option after the first line if not already present.
+          if (!contents.includes('LINKER:--build-id=none')) {
+            const lines = contents.split('\n');
+            lines.splice(1, 0, 'add_link_options("LINKER:--build-id=none")');
+            contents = lines.join('\n');
+            fs.writeFileSync(cmakeFilePath, contents, 'utf8');
+          }
+        }
+      }
+      return config;
+    },
+  ]);
 };
 
-module.exports = withNDKBuildId;
+module.exports = withAddLinkOption;
