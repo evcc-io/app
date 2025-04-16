d138fc42849b64bf21e1ee9c0a18a08321ebaddf
Michael Geers <michael@geers.tv>
2025-04-16 14:28:02 +0200

    feature: add version to change server screen

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
 import { version } from "../package.json";
 
 export const USER_AGENT = `evcc/${version}`;
+export const APP_VERSION = `v${version}`;
+export const GITHUB_RELEASES_URL = `https://github.com/evcc-io/app/releases/tag/${version}`;
