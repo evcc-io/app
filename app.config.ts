6502c79a67b2007dcc41bd8fdcd6daa0bd562cc2
Michael Geers <michael@geers.tv>
2025-09-15 10:16:41 +0200

    version bump

--- app.config.ts
+++ app.config.ts
@@ -34,7 +34,7 @@ export default ({ config }: ConfigContext) =>
         config: {
           usesNonExemptEncryption: false,
         },
-        buildNumber: "31",
+        buildNumber: "32",
       },
       android: {
         jsEngine: "hermes",
@@ -48,7 +48,7 @@ export default ({ config }: ConfigContext) =>
           foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#010322",
         },
-        versionCode: 31,
+        versionCode: 32,
       },
       web: {
         favicon: "./assets/favicon.png",
