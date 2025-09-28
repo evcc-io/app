8528a7c73e280b64939d181a2e1f3ebaf0856dbd
Maschga <delphi@freenet.de>
2025-09-28 11:34:25 +0200

    use ts

---
+++ e2e/jest.config.ts
@@ -0,0 +1,14 @@
+import { Config } from "@jest/types";
+
+module.exports = {
+  preset: "ts-jest",
+  rootDir: "..",
+  testMatch: ["<rootDir>/e2e/**/*.test.ts"],
+  testTimeout: 120000,
+  maxWorkers: 1,
+  globalSetup: "detox/runners/jest/globalSetup",
+  globalTeardown: "detox/runners/jest/globalTeardown",
+  reporters: ["detox/runners/jest/reporter"],
+  testEnvironment: "detox/runners/jest/testEnvironment",
+  verbose: true,
+} satisfies Config.InitialOptions;
