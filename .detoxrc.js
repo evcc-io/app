922fc2a96b48c3d1bb4eba260f4b32d124a9d537
Michael Geers <michael@geers.tv>
2025-12-23 11:46:58 +0100

    create simulator instead of using an existing one

--- .detoxrc.js
+++ .detoxrc.js
@@ -47,7 +47,7 @@ module.exports = {
       type: "ios.app",
       binaryPath: "ios/build/Build/Products/Release-iphonesimulator/evcc.app",
       build:
-        "SIMULATOR_ID=$(xcrun simctl list devices available | grep 'iPhone 16e' | grep '18.6' | head -1 | sed -E 's/.*\\(([A-F0-9-]+)\\).*/\\1/') && xcodebuild -workspace ios/evcc.xcworkspace -scheme evcc -configuration Release -sdk iphonesimulator -destination \"id=$SIMULATOR_ID\" -derivedDataPath ios/build -quiet | xcbeautify --renderer github-actions",
+        "xcodebuild -workspace ios/evcc.xcworkspace -scheme evcc -configuration Release -sdk iphonesimulator -destination 'name=evcc-test' -derivedDataPath ios/build -quiet | xcbeautify --renderer github-actions",
     },
     "android.release": {
       launchArgs,
@@ -60,7 +60,7 @@ module.exports = {
     simulator: {
       type: "ios.simulator",
       device: {
-        type: "iPhone 16e",
+        name: "evcc-test",
       },
     },
     attached: {
