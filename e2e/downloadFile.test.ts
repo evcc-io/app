d2e44892c76cb983ba36cbff5601c4cd295277e4
Maschga <delphi@freenet.de>
2026-01-01 12:47:39 +0100

    use data-testid

--- e2e/downloadFile.test.ts
+++ e2e/downloadFile.test.ts
@@ -16,20 +16,20 @@ describe("Download file", () => {
     await waitForWebview();
 
     await byWebDataTestId("topnavigation-button").tap();
-    await web.element(by.web.label("Charging Sessions")).tap();
-    await web.element(by.web.cssSelector("a[download]")).tap();
+    await byWebDataTestId("topnavigation-sessions").tap();
+    await byWebDataTestId("sessions-download").tap();
 
     await expect(system.element(by.system.label("Save to Files"))).toExist();
   });
 
-  // TODO: add test for android
-  // TODO: unskip and add assertion for file download
-  it.skip("backup file", async () => {
+  // TODO: add assertion for file download
+  it("backup file", async () => {
     await element(by.id("serverFormCheckAndSave")).tap();
     await waitForWebview();
 
-    await web.element(by.web.label("Configuration")).tap();
-    await web.element(by.web.label("Backup & Restore")).tap();
-    await web.element(by.web.label("Download backup...")).tap();
+    await byWebDataTestId("topnavigation-button").tap();
+    await byWebDataTestId("topnavigation-config").tap();
+    await byWebDataTestId("backup-restore").tap();
+    await byWebDataTestId("backup-restore-open-confirm-modal").tap();
   });
 });
