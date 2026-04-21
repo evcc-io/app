57c64414281f133aefba1d9dcff885b507ecf511
Maschga <delphi@freenet.de>
2026-04-21 11:16:14 +0200

    refactor

--- components/MdnsSearch.tsx
+++ components/MdnsSearch.tsx
@@ -1,11 +1,141 @@
-import React from "react";
-import { Button, List, ListItem } from "@ui-kitten/components";
-import { StyleSheet } from "react-native";
-import { useTranslation } from "react-i18next";
+import React, { useCallback, useState } from "react";
+import { Button, Layout, Text } from "@ui-kitten/components";
 import { Connection } from "types";
+import { t } from "i18next";
+import LoadingIndicator from "./animations/LoadingIndicator";
+import ServerList from "./ServerList";
+import * as ServiceDiscovery from "@inthepocket/react-native-service-discovery";
 
-interface MdnsSearchProps {}
+interface MdnsSearchProps {
+  selectServer: (url: string) => Promise<void>;
+}
 
-export default function MdnsSearch({}: MdnsSearchProps): React.ReactElement {
-  return <></>;
+export default function MdnsSearch({
+  selectServer,
+}: MdnsSearchProps): React.ReactElement {
+  const [searching, setSearching] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [scanNotPossible, setScanNotPossible] = useState(false);
+  const [found, setFound] = useState<Connection[]>([]);
+
+  const getTitle = (service: ServiceDiscovery.Service) => {
+    let title = service.hostName;
+    for (const s of [".local.", ".fritz.box"]) {
+      if (title.endsWith(s)) {
+        title = title.slice(0, -1 * s.length);
+        break;
+      }
+    }
+    return title;
+  };
+
+  const getUrl = (service: ServiceDiscovery.Service) => {
+    const scheme = service.type === "_http._tcp." ? "http" : "https";
+    const hostName = service.hostName.endsWith(".")
+      ? service.hostName.slice(0, -1)
+      : service.hostName;
+    const port =
+      service.port === 80 || service.port === 443 ? "" : `:${service.port}`;
+    return `${scheme}://${hostName}${port}`;
+  };
+
+  const toInstance = (service: ServiceDiscovery.Service): Connection => {
+    return { title: getTitle(service), url: getUrl(service) };
+  };
+
+  const sameInstance = (a: Connection, b: Connection) => {
+    return a.url === b.url;
+  };
+
+  const scanNetwork = useCallback(() => {
+    // for multiple clicks on button
+    ServiceDiscovery.stopSearch("http");
+    ServiceDiscovery.stopSearch("https");
+
+    setSearching(true);
+    setFinished(false);
+    setFound([]);
+
+    const foundListener = ServiceDiscovery.addEventListener(
+      "serviceFound",
+      (service: ServiceDiscovery.Service) => {
+        if (service.name === "evcc") {
+          console.log("Found service ", service);
+          setFound((found) => {
+            const instance = toInstance(service);
+            if (!found.some((f) => sameInstance(f, instance))) {
+              return [...found, instance];
+            } else {
+              return found;
+            }
+          });
+        }
+      },
+    );
+
+    const lostListener = ServiceDiscovery.addEventListener(
+      "serviceLost",
+      (service: ServiceDiscovery.Service) => {
+        if (service.name === "evcc") {
+          console.log("Lost service ", service);
+          setFound((found) => {
+            const instance = toInstance(service);
+            return found.filter((f) => !sameInstance(f, instance));
+          });
+        }
+      },
+    );
+
+    (async () => {
+      try {
+        await Promise.all([
+          ServiceDiscovery.startSearch("http"),
+          ServiceDiscovery.startSearch("https"),
+        ]);
+
+        setTimeout(() => {
+          ServiceDiscovery.stopSearch("http");
+          ServiceDiscovery.stopSearch("https");
+
+          foundListener.remove();
+          lostListener.remove();
+
+          setSearching(false);
+          setFinished(true);
+        }, 60 * 1000);
+      } catch (e) {
+        console.log("error", e);
+        setSearching(false);
+        setScanNotPossible(true);
+      }
+    })();
+  }, []);
+
+  return (
+    <Layout>
+      <Button
+        style={{ marginTop: 8, marginBottom: 32 }}
+        appearance="filled"
+        size="giant"
+        onPress={scanNetwork}
+        accessoryLeft={searching ? LoadingIndicator : undefined}
+        disabled={scanNotPossible}
+        testID="serverSearchButton"
+      >
+        {t("servers.search.start")}
+      </Button>
+      {scanNotPossible ? (
+        <Text style={{ marginVertical: 16 }} category="p1">
+          {t("servers.search.notAvailable")}
+        </Text>
+      ) : null}
+      {finished && found.length === 0 ? (
+        <Text style={{ marginVertical: 16 }} category="p1">
+          {t("servers.search.nothingFound")}
+        </Text>
+      ) : (
+        <ServerList entries={Array.from(found)} onSelect={selectServer} />
+      )}
+    </Layout>
+  );
 }
--- screens/ServerScreen.tsx
+++ screens/ServerScreen.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useCallback } from "react";
-import * as ServiceDiscovery from "@inthepocket/react-native-service-discovery";
+import React, { useCallback } from "react";
 import {
   Layout,
   Text,
@@ -10,118 +9,18 @@ import {
 } from "@ui-kitten/components";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Alert } from "react-native";
-
 import { useAppContext } from "../components/AppContext";
-import ServerList from "../components/ServerList";
-import LoadingIndicator from "../components/animations/LoadingIndicator";
 import { verifyEvccServer } from "../utils/server";
 import { useTranslation } from "react-i18next";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Connection, RootStackParamList } from "types";
+import MdnsSearch from "components/MdnsSearch";
 
 export default function ServerScreen({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, "Server">) {
   const { t } = useTranslation();
-  const [searching, setSearching] = useState(false);
-  const [finished, setFinished] = useState(false);
-  const [scanNotPossible, setScanNotPossible] = useState(false);
-  const [found, setFound] = useState<Connection[]>([]);
-
   const { updateConnection } = useAppContext();
-
-  const getTitle = (service: ServiceDiscovery.Service) => {
-    let title = service.hostName;
-    for (const s of [".local.", ".fritz.box"]) {
-      if (title.endsWith(s)) {
-        title = title.slice(0, -1 * s.length);
-        break;
-      }
-    }
-    return title;
-  };
-
-  const getUrl = (service: ServiceDiscovery.Service) => {
-    const scheme = service.type === "_http._tcp." ? "http" : "https";
-    const hostName = service.hostName.endsWith(".")
-      ? service.hostName.slice(0, -1)
-      : service.hostName;
-    const port =
-      service.port === 80 || service.port === 443 ? "" : `:${service.port}`;
-    return `${scheme}://${hostName}${port}`;
-  };
-
-  const toInstance = (service: ServiceDiscovery.Service): Connection => {
-    return { title: getTitle(service), url: getUrl(service) };
-  };
-
-  const sameInstance = (a: Connection, b: Connection) => {
-    return a.url === b.url;
-  };
-
-  const scanNetwork = useCallback(() => {
-    // for multiple clicks on button
-    ServiceDiscovery.stopSearch("http");
-    ServiceDiscovery.stopSearch("https");
-
-    setSearching(true);
-    setFinished(false);
-    setFound([]);
-
-    const foundListener = ServiceDiscovery.addEventListener(
-      "serviceFound",
-      (service: ServiceDiscovery.Service) => {
-        if (service.name === "evcc") {
-          console.log("Found service ", service);
-          setFound((found) => {
-            const instance = toInstance(service);
-            if (!found.some((f) => sameInstance(f, instance))) {
-              return [...found, instance];
-            } else {
-              return found;
-            }
-          });
-        }
-      },
-    );
-
-    const lostListener = ServiceDiscovery.addEventListener(
-      "serviceLost",
-      (service: ServiceDiscovery.Service) => {
-        if (service.name === "evcc") {
-          console.log("Lost service ", service);
-          setFound((found) => {
-            const instance = toInstance(service);
-            return found.filter((f) => !sameInstance(f, instance));
-          });
-        }
-      },
-    );
-
-    (async () => {
-      try {
-        await Promise.all([
-          ServiceDiscovery.startSearch("http"),
-          ServiceDiscovery.startSearch("https"),
-        ]);
-
-        setTimeout(() => {
-          ServiceDiscovery.stopSearch("http");
-          ServiceDiscovery.stopSearch("https");
-
-          foundListener.remove();
-          lostListener.remove();
-
-          setSearching(false);
-          setFinished(true);
-        }, 60 * 1000);
-      } catch (e) {
-        console.log("error", e);
-        setSearching(false);
-        setScanNotPossible(true);
-      }
-    })();
-  }, []);
 
   const selectDemoServer = useCallback(async () => {
     await selectServer("https://demo.evcc.io/");
@@ -160,31 +59,8 @@ export default function ServerScreen({
             {t("main.description")}
           </Text>
 
-          <Layout>
-            <Button
-              style={{ marginTop: 8, marginBottom: 32 }}
-              appearance="filled"
-              size="giant"
-              onPress={scanNetwork}
-              accessoryLeft={searching ? LoadingIndicator : undefined}
-              disabled={scanNotPossible}
-              testID="serverSearchButton"
-            >
-              {t("servers.search.start")}
-            </Button>
-            {scanNotPossible ? (
-              <Text style={{ marginVertical: 16 }} category="p1">
-                {t("servers.search.notAvailable")}
-              </Text>
-            ) : null}
-            {finished && found.length === 0 ? (
-              <Text style={{ marginVertical: 16 }} category="p1">
-                {t("servers.search.nothingFound")}
-              </Text>
-            ) : (
-              <ServerList entries={Array.from(found)} onSelect={selectServer} />
-            )}
-          </Layout>
+          <MdnsSearch selectServer={selectServer} />
+
           <Divider style={{ backgroundColor: "white" }} />
           <Layout>
             <Text
