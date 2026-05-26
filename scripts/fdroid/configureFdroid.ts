80d3573a78ba600d244cee6719b6f8c030e41066
Maschga <delphi@freenet.de>
2026-05-26 14:38:03 +0200

    fix typo

--- scripts/fdroid/configureFdroid.ts
+++ scripts/fdroid/configureFdroid.ts
@@ -5,13 +5,13 @@ import {
 } from "expo/config-plugins";
 import disableDependencyMetadata from "./src/disableDependencyMetadata";
 import excludeNonfreeDependencies from "./src/excludeNonfreeDependencies";
-import removeDKBuildId from "./src/removeDKBuildId";
+import removeNDKBuildId from "./src/removeNDKBuildId";
 
 const configureFdroid: ConfigPlugin = (config) => {
   return withPlugins(config, [
     disableDependencyMetadata,
     excludeNonfreeDependencies,
-    removeDKBuildId,
+    removeNDKBuildId,
   ]);
 };
 
