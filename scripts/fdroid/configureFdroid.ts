f26463ca536e1da7d5b2a8ef2c0873799ea95f52
Maschga <delphi@freenet.de>
2026-05-26 15:54:53 +0200

    add removeExpScheme plugin

--- scripts/fdroid/configureFdroid.ts
+++ scripts/fdroid/configureFdroid.ts
@@ -6,12 +6,14 @@ import {
 import disableDependencyMetadata from "./src/disableDependencyMetadata";
 import excludeNonfreeDependencies from "./src/excludeNonfreeDependencies";
 import removeNDKBuildId from "./src/removeNDKBuildId";
+import removeExpScheme from "./src/removeExpScheme";
 
 const configureFdroid: ConfigPlugin = (config) => {
   return withPlugins(config, [
     disableDependencyMetadata,
     excludeNonfreeDependencies,
     removeNDKBuildId,
+    removeExpScheme,
   ]);
 };
 
