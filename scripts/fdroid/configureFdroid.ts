4de11fba9628d8aa5718d6a8c370f07f93a73e25
Maschga <delphi@freenet.de>
2026-05-30 17:22:03 +0200

    remove unecessary fdroid scripts

--- scripts/fdroid/configureFdroid.ts
+++ scripts/fdroid/configureFdroid.ts
@@ -4,17 +4,9 @@ import {
   withPlugins,
 } from "expo/config-plugins";
 import disableDependencyMetadata from "./src/disableDependencyMetadata";
-import excludeNonfreeDependencies from "./src/excludeNonfreeDependencies";
-import removeNDKBuildId from "./src/removeNDKBuildId";
-import removeExpScheme from "./src/removeExpScheme";
 
 const configureFdroid: ConfigPlugin = (config) => {
-  return withPlugins(config, [
-    disableDependencyMetadata,
-    excludeNonfreeDependencies,
-    removeNDKBuildId,
-    removeExpScheme,
-  ]);
+  return withPlugins(config, [disableDependencyMetadata]);
 };
 
 export default createRunOncePlugin(configureFdroid, "configure-fdroid");
