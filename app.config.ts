435bd0aa351c0d6ba0d0be2afa29b8c96d7876dc
Michael Geers <michael@geers.tv>
2026-08-03 12:32:39 +0200

    bump

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext) =>
       slug: "evcc",
       scheme: SCHEME,
       description: "open source solar charging",
-      version: "1.4.0",
+      version: "1.4.1",
       orientation: "default",
       icon: "./assets/icon-light.png",
       userInterfaceStyle: "automatic",
@@ -40,7 +40,7 @@ export default ({ config }: ConfigContext) =>
         config: {
           usesNonExemptEncryption: false,
         },
-        buildNumber: "46",
+        buildNumber: "47",
       },
       android: {
         permissions: [
@@ -53,7 +53,7 @@ export default ({ config }: ConfigContext) =>
           foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#010322",
         },
-        versionCode: 46,
+        versionCode: 47,
       },
       web: {
         favicon: "./assets/favicon.png",
